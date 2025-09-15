1de162c7c4fd5ebfa0b6c133e3404bb4896e3a36
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-09-15 21:17:30 +0800

    Convert to html

---
+++ php/menu.php
@@ -0,0 +1,39 @@
+<div class="sidebar-container col-sm-3" id="menuEl">
+
+    <div class="container">
+        <div class="sidebar-content-cont d-flex flex-column justify-content-between">
+            <div class="top-section">
+                <div class="mobile-header">
+                    <h3 class="fw-semibold">Lorenz Ciocon</h3>
+
+                    <div class="burger" id="burgerEl">
+                        <div class="line1"></div>
+                        <div class="line2"></div>
+                        <div class="line3"></div>
+                    </div>
+                </div>
+                
+
+                <ul>
+                    <li class="mt-3"><a href="index.php">Home</a></li>
+                    <li class="mt-3"><a href="aboutPage.php">About</a></li>
+                    <li class="mt-3"><a href="projectsPage.php">Projects</a></li>
+                </ul>
+            </div>
+            
+            <div class="bot-section ">
+                <p id="date-el">August 26, 2025</p>
+
+                <div class="d-flex justify-content-between align-items-center">
+                    <a href="https://www.facebook.com/Ensssso" class="mx-2"><img src="./img/icons8-facebook-24.png" alt=""></a>
+                    <a href="https://github.com/liliput35" class="mx-2"><img src="./img/icons8-github-24.png" alt=""></a>
+                    <a href="mailto:cioconlorenz@gmail.com" class="mx-2"><img src="./img/icons8-mail-24.png" alt=""></a>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    
+</div>
